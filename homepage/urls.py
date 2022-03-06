784fa9ad93ea34fbd42c2d5ecdb17a81b768f6b9
abhishekY2401 <abhiyd2401@gmail.com>
2022-03-06 17:35:49 +0530

    changed the urls

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,5 +1,5 @@
 
-from django.urls import path
+from django.urls import path, include
 from register.views import registerPage
 from homepage.views import homePage
 from dashboard.views import dashboardPage
@@ -7,5 +7,5 @@ from dashboard.views import dashboardPage
 urlpatterns = [
     path('', homePage, name="homepage"),
     path('dashboard/', dashboardPage, name="dashboard"),
-    path('register/', registerPage, name="register"),
+    path('register/', registerPage, name="register")
 ]
--- order/views.py
+++ order/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def addOrderPage(request):
+    return render(request, 'addOrder.html')
+
+
+def orderPage(request):
+    return render(request, 'order.html')
