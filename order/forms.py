9f23b517f598bd73920df6f04e39235d1d3f4c4e
abhishekY2401 <abhiyd2401@gmail.com>
2022-05-31 19:34:10 +0530

    deleted orders folder

--- order/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Order
-
-
-class OrderForm(ModelForm):
-    class Meta:
-        model = Order
-        fields = '__all__'
--- order/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from order.views import detailedOrderPage
-
-urlpatterns = [
-    path('order/<str:id>/', detailedOrderPage, name="order"),
-]
