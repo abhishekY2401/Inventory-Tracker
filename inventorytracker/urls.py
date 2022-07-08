07a61e7d22ebacd7bb08459575f211b5a2457fd1
abhishekY2401 <abhiyd2401@gmail.com>
2022-07-08 11:10:57 +0530

    changed

--- inventorytracker/urls.py
+++ inventorytracker/urls.py
@@ -19,16 +19,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.views.static import serve
-from django.conf.urls import url
 
 app_name = 'dashboard'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
     path('dashboard/', include('dashboard.urls')),
-
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
 ]
