56cd208254587e6fc258499e0ed05f15348375bc
Abhishek Yadav <89199323+abhishekY2401@users.noreply.github.com>
2023-01-14 15:09:58 +0530

    changed the url in the hosts array

--- inventorytracker/settings.py
+++ inventorytracker/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # styling config for forms
